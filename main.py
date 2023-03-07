03c5ad6dc231b9ae1ad958e4862ed6ec0b931359
Nikolay <nstrekopytov@gmail.com>
2023-03-07 12:23:09 +0300

    MVP version

--- main.py
+++ main.py
@@ -1,85 +1,99 @@
 import numpy as np
-import matplotlib.pyplot as plt
-# import matplotlib.cm as cm
-# import pandas as pd
-import time
-import matplotlib.animation as animation
-import matplotlib as mpl
-
-mpl.use('TkAgg')
-
-NUMBER_X: int = 2
-NUMBER_Y: int = 1
-
-CANVAS_WIDTH:  int = 10
-CANVAS_HEIGHT: int = 10
-
-def heatmap_animation1():
-    fig, ax_lst = plt.subplots(NUMBER_X, NUMBER_Y)
-    ax_lst = ax_lst.ravel()
-
-    def plot(data):
-        data = np.random.rand(CANVAS_WIDTH, CANVAS_HEIGHT)
-        heatmap = ax_lst[0].pcolor(data)
-
-    ani = animation.FuncAnimation(fig, plot, interval=1)
-    plt.show()
-
-def heatmap_animation2():
-    fig, ax_lst = plt.subplots(NUMBER_X, NUMBER_Y)
-    ax_lst = ax_lst.ravel()
-
-    data = np.random.rand(CANVAS_WIDTH, CANVAS_HEIGHT)
-    im = ax_lst[0].imshow(data)
-
-    while True:
-        t_start = time.time()
-        data = np.random.rand(CANVAS_WIDTH, CANVAS_HEIGHT)
-        im.set_data(data)
-        plt.pause(0.001)
-        t_end = time.time()
-        print("fps = {0}".format(999 if t_end == t_start else 1/(t_end-t_start)))
-
-def heatmap_animation3():
-    fig, ax_lst = plt.subplots(NUMBER_X, NUMBER_Y)
-    ax_lst = ax_lst.ravel()
-
-    data = np.random.rand(CANVAS_WIDTH, CANVAS_HEIGHT)
-    heatmap = ax_lst[0].pcolor(data)
-    fig.canvas.draw()
-    fig.show()
-
-    while True:
-        data = np.random.rand(CANVAS_WIDTH, CANVAS_HEIGHT)
-        t_start = time.time()
-        heatmap = ax_lst[0].pcolor(data)
-        ax_lst[0].draw_artist(ax_lst[0].patch)
-        ax_lst[0].draw_artist(heatmap)
-        fig.canvas.blit(ax_lst[0].bbox)
-        fig.canvas.flush_events()
-        t_end = time.time()
-        print("fps = {0}".format(999 if t_end == t_start else 1/(t_end-t_start)))
 
 
-def main():
-    """
-    Entry function
-    :called when: the program starts
-    :param none: no parameter
-    :return: none
-    :rtype: none
-    """
-    heatmap_animation3()
+# TODO: nodes_to_update - параметр задающий частоту обновления графика
 
 
+class NodesGenerator:
+    def __init__(self, start, end, degree:int, func):
+        self.start = start
+        self.end = end
+        self.degree = degree
+        self.length = 2 ** degree
+        self.func = func
 
-if __name__ == '__main__':
-    main()
+        self.prev_nodes = None
+        self.curr_nodes = None
 
+        self.array_x = np.linspace(start, end, self.length)
+        self.array_y = np.full_like(self.array_x, np.nan, dtype=np.double)
+        self.mask = np.full_like(self.array_x, False, dtype=bool)
 
-class CalculationProcess: # или AnimationProcess или ProgressiveHeatmap
-    # Здесь вычислять значения в узлах для истинной таблицы,
-    # выбирать следующие координаты
-    # и модифицировать значения ускоренной таблицы
-    def step(self):
-        pass
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        self.array_y[0] = self.func(self.array_x[0])
+        self.mask[0] = True
+        self.array_y[-1] = self.func(self.array_x[-1])
+        self.mask[-1] = True
+
+        self.prev_nodes = [(0, -1 + 2 ** self.degree)]
+        self.curr_nodes = []
+        self.n = 1
+        self.next_index_generator = self.next_index()
+        return self
+
+    def __next__(self):
+        if self.n == self.length:
+            raise StopIteration
+
+        index = next(self.next_index_generator)
+        # print(self.n, index)
+        index = int(index)
+
+        self.n += 1
+
+        x = self.array_x[index]
+        self.array_y[index] = self.func(x)
+        self.mask[index] = True
+
+        # print(*self.array_y)
+        # if self.n > 120:
+        #     print(*self.array_y)
+        #     print(*self.array_y[self.mask])
+        return self.array_x[self.mask], self.array_y[self.mask]
+
+    def next_index(self):
+
+        for _ in range(self.degree):
+
+            for node in self.prev_nodes:
+                c = sum(node) / 2
+
+                # c = np.rint(c)
+
+                # if c - node[0] > 1:
+                #     self.curr_nodes.append((node[0], c))
+                #
+                # if node[1] - c > 1:
+                #     self.curr_nodes.append((c, node[1]))
+                self.curr_nodes.extend([(node[0], c), (c, node[1])])
+
+                yield c
+
+            self.prev_nodes = list(self.curr_nodes)
+            self.curr_nodes = []
+
+# fig, ax = plt.subplots()
+# xdata, ydata = [], []
+# ln, = ax.plot([], [], 'ro')
+#
+#
+# def init():
+#     ax.set_xlim(0, 2 * 2 * np.pi)
+#     ax.set_ylim(-3, 3)
+#     return ln,
+#
+#
+# def update(frame):
+#     # xdata.append(frame[0])
+#     # ydata.append(frame[1])
+#     # ln.set_data(xdata, ydata)
+#     # print(frame)
+#     return ax.plot(frame[0], frame[1])
+
+# gen = NodesGenerator(start=0, end=2 * 2 * np.pi, degree=7, func=lambda x: np.sin(x) + np.sin(2 * x))
+#
+# ani = FuncAnimation(fig, update, frames=gen, init_func=init, blit=True, repeat=False)
+# plt.show()
--- usage_example.py
+++ usage_example.py
@@ -1,13 +1,40 @@
+# progressive_plot = ProgressivePlot()
+#
+# # Надо задать функцию и сетку узлов
+#
+# def f(x):
+#     return x  # some function
+#
+#
+# M = EquidistantNodesSet1d(start, end, step)
+#
+# progressive_plot.start(f, M)
+
+from main import NodesGenerator
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+mpl.use('TkAgg')
+
+# nodes_to_update - параметр задающий частоту обновления графика
+
+fig, ax = plt.subplots()
+ln, = ax.plot([], [], 'ro')
 
 
-progressive_plot = ProgressivePlot()
-
-# Надо задать функцию и сетку узлов
-
-def f(x):
-    return x  # some function
+def init():
+    ax.set_xlim(0, 2 * 2 * np.pi)
+    ax.set_ylim(-3, 3)
+    return ln,
 
 
-M = EquidistantNodesSet1d(start, end, step)
+def update(frame):
+    return ax.plot(frame[0], frame[1], color='blue')
 
-progressive_plot.start(f, M)
+
+gen = NodesGenerator(start=0, end=2 * 2 * np.pi, degree=7, func=lambda x: np.sin(x) + np.sin(2 * x))
+
+ani = FuncAnimation(fig, update, frames=gen, init_func=init, blit=True, repeat=False)
+plt.show()
